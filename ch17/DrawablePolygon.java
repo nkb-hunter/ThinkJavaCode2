ae0e0501d0681685aa33b1074bdb4f211a3651df
nkb-hunter <naomi.keuschbaker41@students.huntersoe.org>
2024-11-07 13:32:54 -0500

    toString

--- ch17/DrawablePolygon.java
+++ ch17/DrawablePolygon.java
@@ -18,6 +18,18 @@ public class DrawablePolygon extends Polygon implements Actor {
     }
 
     /**
+     * Returns a string of point coordinates and color.
+     */
+    public String toString(){
+        String result = "A " + color +" polygon with the following points: ";
+        for(int i = 0; i < xpoints.length; i++){
+            result += "(" + xpoints[i] + ", " + ypoints[i] + ")";
+        }
+        return result;
+    }
+
+
+    /**
      * Draws the polygon on the screen.
      * 
      * @param g graphics context
@@ -43,6 +55,7 @@ public class DrawablePolygon extends Polygon implements Actor {
         p.addPoint(100, 35);
         p.addPoint(143, 110);
         p.color = Color.GREEN;
+        System.out.println(p);
     }
 
 }
